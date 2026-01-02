b62f34fe34833dfdcee4634ed7d16fac1b33629a
nestaidofficial <nestaidofficial60@gmail.com>
2026-01-01 19:57:08 -0500

    Fix Netlify image deployment: add @netlify/plugin-nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: false,
   },
 }
 
